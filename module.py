80a0e4b8f398abc796d31e73f16424954a42656c
lovelaced <erin@parity.io>
2022-12-23 14:59:17 +0000

    some fixes

--- module.py
+++ module.py
@@ -7,16 +7,16 @@ from twisted.web.server import Request
 from synapse.module_api import ModuleApi
 
 class MayInviteResource(Resource):
-    def __init__(self, config, spam_checker):
+    def __init__(self, config):
         super(MayInviteResource, self).__init__()
         self.config = config
 
     def render_GET(self, request: Request):
         request.setHeader(b"Content-Type", b"application/json")
-        return json.dumps({"message": "This identity doesn't accept public invitations. If you need to get in touch, please use " + config.shielded_user.email + ". Kind regards."})
+        return json.dumps({"message": "This identity doesn't accept public invitations. If you need to get in touch, please use " + self.config.shielded_user.email + ". Kind regards."})
 
 
-class MayInviteModule:
+class SynapseMayInviteModule:
     def __init__(self, config: dict, api: ModuleApi):
         self.config = config
         self.api = api
@@ -31,11 +31,9 @@ class MayInviteModule:
     @staticmethod
     def parse_config(config):
         return config
-
-    async def user_may_invite(self, sender: str) -> bool:
+    async def user_may_invite(self, sender: str, target: str, shielded_user: UserID) -> bool:
         # Check if the user trying to invite is from a different homeserver
         if sender != self.api.hs.hostname:
             if target == shielded_user.mxid:
                 return False
         return True
-
