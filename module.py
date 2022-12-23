b81bf4d604bbee75b7bee2a091cdfb1762ed7a32
lovelaced <erin@parity.io>
2022-12-23 16:17:34 +0000

    add multi user support

--- module.py
+++ module.py
@@ -6,6 +6,11 @@ from twisted.web.resource import Resource
 from twisted.web.server import Request
 from synapse.module_api import ModuleApi
 
+class ShieldedUser:
+    def __init__(self, mxid: str, email: str):
+        self.mxid = mxid
+        self.email = email
+
 class MayInviteResource(Resource):
     def __init__(self, config):
         super(MayInviteResource, self).__init__()
@@ -28,12 +33,20 @@ class SynapseMayInvite:
         self.api.register_spam_checker_callbacks(
             user_may_invite=self.user_may_invite,
         )
+
     @staticmethod
     def parse_config(config):
+        shielded_users = []
+        for user in config["shielded_users"]:
+            shielded_users.append(ShieldedUser(user["mxid"], user["email"]))
+        config["shielded_users"] = shielded_users
         return config
-    async def user_may_invite(self, sender: str, target: str, shielded_user: UserID) -> bool:
+
+    async def user_may_invite(self, sender: str, target: str, shielded_users: List[ShieldedUser]) -> bool:
         # Check if the user trying to invite is from a different homeserver
         if sender != self.api.hs.hostname:
-            if target == shielded_user.mxid:
-                return False
+            # Check if the target user is in the list of shielded users
+            for shielded_user in shielded_users:
+                if target == shielded_user.mxid:
+                    return False
         return True
