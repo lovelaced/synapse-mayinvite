0135f7b4a8f208b94d66253e944a82314e1c9983
lovelaced <erin@parity.io>
2023-04-21 15:22:29 +0200

    update to new module api

--- module.py
+++ module.py
@@ -1,41 +1,18 @@
 import json
 import logging
 
-from synapse.api.errors import SynapseError
+from synapse.api.errors import SynapseError, Codes
 from synapse.types import UserID
-from twisted.web.resource import Resource
-from twisted.web.server import Request
-from synapse.module_api import ModuleApi
+from synapse.module_api import ModuleApi, NOT_SPAM
 from typing import Dict
 
 logger = logging.getLogger(__name__)
-
-class MayInviteResource(Resource):
-    def __init__(self, config):
-        # Initialize the base Resource class
-        super(MayInviteResource, self).__init__()
-        # Store the configuration
-        self.config = config
-
-    def render_GET(self, request: Request):
-        # Set the content type of the response to application/json
-        request.setHeader(b"Content-Type", b"application/json")
-        # Get the target user's Matrix ID from the request arguments
-        target = request.args[b"target"][0].decode("utf-8")
-        # Return a message indicating that the user doesn't accept public invitations
-        return json.dumps({"message": "This identity doesn't accept public invitations. If you need to get in touch, please use " + self.config["shielded_users"][target] + ". Kind regards."})
 
 class SynapseMayInvite:
     def __init__(self, config: dict, api: ModuleApi):
         # Store the configuration and API object
         self.config = config
         self.api = api
-
-        # Register the user_may_invite callback function to be called for every invite request
-        self.api.register_spam_checker_callbacks(
-            user_may_invite=self.user_may_invite,
-        )
-        logger.info("Registered spam checker callbacks")
 
     @staticmethod
     def parse_config(config: dict) -> dict:
@@ -48,7 +25,7 @@ class SynapseMayInvite:
         config["allowed_homeservers"] = config.get("allowed_homeservers", [])
         return config
 
-    async def user_may_invite(self, sender: str, target: str, room_id: str) -> bool:
+    async def user_may_invite(self, sender: str, target: str, room_id: str):
         logger.info("Shielded users:")
         logger.info(self.config["shielded_users"])
         logger.info("Allowed homeservers:")
@@ -57,18 +34,18 @@ class SynapseMayInvite:
         # Check if the user trying to invite is from a different homeserver
         if self.api.is_mine(sender):
             # Allow the invite if the sender is from the same homeserver
-            logger.info("Allowed invite from %s to %s to %s", sender, target)
-            return True
+            logger.info(f"Allowed invite from {sender} to {target}")
+            return NOT_SPAM
         # Check if the homeserver of the sender is in the list of allowed homeservers
         sender_homeserver = UserID.from_string(sender).domain
         if sender_homeserver in self.config["allowed_homeservers"]:
             # Allow the invite if the sender is from an allowed homeserver
-            logger.info("Allowed invite from %s to %s", sender, target)
-            return True
+            logger.info(f"Allowed invite from {sender} to {target}")
+            return NOT_SPAM
         # Check if the target user is in the list of shielded users
         if target in shielded_mxids:
-            logger.info("Blocked invite from %s to shielded user %s", sender, target)
-            return False
-        logger.info("Allowed invite from %s to %s", sender, target)
-        return True
+            logger.info(f"Blocked invite from {sender} to shielded user {target}")
+            return Codes.FORBIDDEN
+        logger.info(f"Allowed invite from {sender} to {target}")
+        return NOT_SPAM
 
